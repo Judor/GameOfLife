ef589892c75342c76b9980709750850dc46dd0d3
guillaume.lochontours@gmail.com <bzb5xyu*cek8prw@JHK>
2021-12-09 16:14:14 +0100

    Compute_number_neighbors OK

--- main.py
+++ main.py
@@ -5,19 +5,25 @@
 
 import numpy as np
 
-frame = np.array([0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 1, 1, 1, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0])
+frame = np.array([[0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 1, 1, 1, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0]])
 
 
 def compute_number_neighbors(paded_frame, index_line, index_column):
-    number_neighbors = 0
-    # TODO --> Create a function that takes in entry a frame with borders and returns the number of cells of a
-    #  neighbors
+    number_neighbors = 0  # Creation of the
+
+    for i in range(index_line - 1, index_line + 2):
+        for j in range(index_column - 1, index_column + 2):
+            number_neighbors = number_neighbors + paded_frame[i][j]
+
+    number_neighbors = number_neighbors - paded_frame[index_line, index_column]  # Suppression of cell in entry
+
+    # TODO --> Optimize the function to skip the cell in entry
     return number_neighbors
 
 
@@ -33,8 +39,7 @@ def compute_next_frame(frame):
     #    frame. If it's the case, update the element in the matrix
     return frame
 
-
-while True:
-    # Infinite loop that print all the frames . Ctrl + C to stop the computation
-    print(frame)
-    frame = compute_next_frame
+# while True:
+#   # Infinite loop that print all the frames . Ctrl + C to stop the computation
+#  print(frame)
+# frame = compute_next_frame
